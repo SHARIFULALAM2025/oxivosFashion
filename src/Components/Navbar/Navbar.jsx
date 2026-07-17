7b73de8d875586c8711fe4156e5fec9dd6b78303
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2026-07-17 23:38:17 +0600

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
         </Link>
         <Button
           className="flex items-center gap-1 text-xs bg-primary text-foreground px-3 py-1.5 rounded-md"
-          href="#"
+          href="/register"
           iconPosition="right"
           icon={<LuLogIn className="text-base" />}
         >
